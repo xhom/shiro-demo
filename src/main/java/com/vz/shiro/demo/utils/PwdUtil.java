57e8332db57e501eb10f91a9db2f46c03fea6310
wangxh <wangxiaohong@kuihai.net>
2023-06-01 18:49:27 +0800

    优化LOG

--- src/main/java/com/vz/shiro/demo/utils/PwdUtil.java
+++ src/main/java/com/vz/shiro/demo/utils/PwdUtil.java
@@ -10,6 +10,7 @@ import org.apache.shiro.crypto.hash.Sha256Hash;
  * @date 2023/5/30 18:16
  */
 public class PwdUtil {
+    private static final String SALT_PREFIX = "$";
     private static final SecureRandomNumberGenerator secureRandomNumberGenerator = new SecureRandomNumberGenerator();
     /**
      * 随机获取一个盐值
@@ -27,5 +28,49 @@ public class PwdUtil {
      */
     public static String encrypt(String password, Object salt){
         return new Sha256Hash(new Md5Hash(password), salt, 2).toHex();
+    }
+
+    /**
+     * 密码加密（自动加盐）
+     * @param rawPassword 原密码（明文）
+     * @return 密文
+     */
+    public static String encrypt(String rawPassword){
+        return encrypt(rawPassword, 10);
+    }
+    /**
+     * 密码加密（自动加盐）
+     * @param rawPassword 原密码（明文）
+     * @param strength 密码强度
+     * @return 密文
+     */
+    public static String encrypt(String rawPassword,  int strength){
+        String realSalt = getSalt();
+        int saltLen = realSalt.length();
+        String saltLenStr = (saltLen>9?"":"0") + saltLen;
+        String strengthStr = (strength>9?"":"0") + strength;
+        String salt = SALT_PREFIX + strengthStr + saltLenStr + realSalt;
+        return encode(rawPassword, salt);
+    }
+
+    private static String encode(String rawPassword,  String salt){
+        String strengthStr = salt.substring(SALT_PREFIX.length(), SALT_PREFIX.length()+2);
+        String saltLenStr = salt.substring(SALT_PREFIX.length()+2, SALT_PREFIX.length()+4);
+        int strength = Integer.parseInt(strengthStr), saltLen = Integer.parseInt(saltLenStr);
+        String realSalt = salt.substring(SALT_PREFIX.length()+4, saltLen);
+        System.out.println("salt: " + salt);
+        System.out.println("strength: " + strength);
+        System.out.println("realSalt: " + realSalt);
+        return salt + new Sha256Hash(new Md5Hash(rawPassword), realSalt, strength).toHex();
+    }
+
+    /**
+     * 密码对比，结合encrypt(String rawPassword,  int strength)使用
+     * @param rawPassword 明文
+     * @param encodedPassword 密文
+     * @return 是否匹配
+     */
+    public static boolean  matches(String rawPassword, String encodedPassword){
+        return encodedPassword.equals(encode(rawPassword, encodedPassword));
     }
 }
